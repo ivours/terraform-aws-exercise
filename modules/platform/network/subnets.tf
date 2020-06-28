b4db60b37b8128e4909b841fe86ab85d0bbe961b
Ivo Ursino <ursinoivo@gmail.com>
2020-06-28 19:02:08 -0300

    Add private subnets route to NAT GW

--- modules/platform/network/subnets.tf
+++ modules/platform/network/subnets.tf
@@ -34,5 +34,25 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public" {
   count          = length(var.availability_zones)
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public .id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.availability_zones)
+  vpc_id = aws_vpc.main.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }

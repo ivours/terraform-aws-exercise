f4c06d3e1004eb9f4182f2d91281a3450339dc17
Ivo Ursino <ursinoivo@gmail.com>
2020-06-28 19:04:39 -0300

    Add SSH access to EC2 instances from VPC

--- modules/business-units/my-web-app/security_groups.tf
+++ modules/business-units/my-web-app/security_groups.tf
@@ -47,3 +47,28 @@ resource "aws_security_group" "allow_http_ec2" {
     Name = "allow_http"
   }
 }
+
+resource "aws_security_group" "allow_ssh_ec2" {
+  name        = "allow_ssh_ec2"
+  description = "Allow SSH to EC2 instances."
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_ec2"
+  }
+}
